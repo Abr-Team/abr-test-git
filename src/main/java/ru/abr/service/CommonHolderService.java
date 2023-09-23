f9da2ded4714eb57381a4784ecef9c72a74130d1
Barsukov Nikita <n.barsukov@abr.ru>
2023-09-23 16:36:57 +0300

    positiion risk done

--- src/main/java/ru/abr/service/CommonHolderService.java
+++ src/main/java/ru/abr/service/CommonHolderService.java
@@ -1,8 +1,12 @@
 package ru.abr.service;
 
 
+import java.util.List;
+
 public interface CommonHolderService {
 
     Object getIncomeAsyncReq(String apiKey, Long id);
+
+    List<Object> getPositionRisk(String symbol, String apiKey);
 
 }
